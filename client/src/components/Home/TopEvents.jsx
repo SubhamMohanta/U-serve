780f6597fce844a001aaf1acf7644164be76f1cb
HiMashuguptA <sg707012@gmail.com>
2024-03-23 06:54:32 +0530

    Added carousel

--- client/src/components/Home/TopEvents.jsx
+++ client/src/components/Home/TopEvents.jsx
@@ -1,33 +1,51 @@
-import { Box, Heading } from '@chakra-ui/react'
-import React from 'react'
-import Event1 from "../../assets/Event1.png"
+import React, { useState, useEffect } from 'react';
+import { Box } from '@chakra-ui/react';
+import Event1 from "../../assets/Event1.png";
 
 const TopEvents = () => {
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const images = [Event1, Event1, Event1, Event1, Event1]; // Add more images as needed
+    const gapBetweenImages = -100; // Adjust this value to decrease the gap between images
 
     return (
         <Box bg="#FFFDF1">
-            {/* <Heading size="lg" textAlign="center" color={"#363636"} mt={10}>Highlights</Heading> */}
-
-            <Box bg="#FFFDF1" display="flex" overflowX="auto" maxW={"100vw"} mx="0" sx={{ '&::-webkit-scrollbar': { display: "none" } }} gap={5} pt={'3%'} pl={"5%"}>
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
-                <img src={Event1} height={'350px'} style={{ borderRadius: "25px" }} />
+            <Box
+                bg="#FFFDF1"
+                display="flex"
+                overflowX="hidden"
+                maxW="100vw"
+                mx="0"
+                pt="3%"
+                pl="5%"
+                sx={{ '&::-webkit-scrollbar': { display: 'none' } }}
+                position="relative"
+            >
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        height="350px"
+                        style={{
+                            borderRadius: '25px',
+                            marginRight: `${gapBetweenImages}px`, // Adjust the marginRight here
+                            transform: `translateX(${(index - currentSlideIndex) * (370 + gapBetweenImages)}px)`,
+                            transition: 'transform 0.5s ease-in-out',
+                            left: `${index * (370 + gapBetweenImages)}px`
+                        }}
+                    />
+                ))}
             </Box>
         </Box>
-    )
-}
+    );
+};
 
-export default TopEvents
+export default TopEvents;
