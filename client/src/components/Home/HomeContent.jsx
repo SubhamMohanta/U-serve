73e2d0173cfcc8b4f1fc722d16a2ff595d42d397
HiMashuguptA <sg707012@gmail.com>
2024-03-23 16:02:31 +0530

    Animation Done

--- client/src/components/Home/HomeContent.jsx
+++ client/src/components/Home/HomeContent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import img1 from "../../assets/Group1.png"
 import img2 from "../../assets/Group 2.png"
 import img3 from "../../assets/Group 3.png"
@@ -6,17 +6,22 @@ import img4 from "../../assets/Group 4.png"
 import { Flex, Heading, Box, Text } from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/image'
 import { Button } from '@chakra-ui/react';
+import 'aos/dist/aos.css'
+import Aos from 'aos';
 
 const HomeContent = () => {
+    useEffect(()=>{
+        Aos.init({duration:2000})
+    },[])
     return (
         <>
             <Box pt="8%" bg="#FFFEF8">
                 <Flex justifyContent={"space-center"} alignItems={"center"}>
                     <Flex align={"between"} justify={"space-between"} m={"15vh 15vw 15vh 10vw"}>
 
-                        <Image src={img1} w={"40%"}></Image>
+                        <Image src={img1} w={"40%"} data-aos="fade-zoom-in"></Image>
 
-                        <Box my={"2%"} textAlign={"end"}>
+                        <Box my={"2%"} textAlign={"end"} data-aos="fade-zoom-in">
                             <Heading fontSize={"48px"}>Volunteer Oppurtunity</Heading>
                             <Heading fontSize={"48px"}>MarketPlace</Heading>
                             <Text color="#363636" fontWeight="500" mt={"3%"} paddingLeft={"40%"} textAlign={"end"}>
@@ -45,7 +50,7 @@ const HomeContent = () => {
                 <Flex justifyContent={"space-center"} alignItems={"center"}>
                     <Flex align={"between"} justify={"space-between"} m={"15vh 10vw 15vh 15vw"}>
 
-                        <Box my={"2%"} textAlign={"start"}>
+                        <Box my={"2%"} textAlign={"start"} data-aos="fade-zoom-in">
                             <Heading fontSize={"48px"}>Community Events</Heading>
                             <Heading fontSize={"48px"}>and Projects</Heading>
                             <Text color="#363636" fontWeight="500" paddingRight={"40%"} mt={"3%"} textAlign={"start"}>
@@ -67,7 +72,7 @@ const HomeContent = () => {
                                 Events →
                             </Button>
                         </Box>
-                        <Image src={img2} w={"40%"}></Image>
+                        <Image src={img2} w={"40%"} data-aos="fade-zoom-in"></Image>
                     </Flex>
                 </Flex>
 
@@ -76,9 +81,9 @@ const HomeContent = () => {
                 <Flex justifyContent={"space-center"} alignItems={"center"}>
                     <Flex align={"between"} justify={"space-between"} m={"15vh 15vw 15vh 10vw"}>
 
-                        <Image src={img3} w={"40%"}></Image>
+                        <Image src={img3} w={"40%"} data-aos="fade-zoom-in"></Image>
 
-                        <Box my={"2%"} textAlign={"end"}>
+                        <Box my={"2%"} textAlign={"end"} data-aos="fade-zoom-in">
                             <Heading fontSize={"48px"}>Volunteer Recognition</Heading>
                             <Heading fontSize={"48px"}>and Rewards</Heading>
                             <Text color="#363636" fontWeight="500" paddingLeft={"40%"} mt={"3%"} textAlign={"end"}>
@@ -92,7 +97,7 @@ const HomeContent = () => {
                 <Flex justifyContent={"space-center"} alignItems={"center"}>
                     <Flex align={"between"} justify={"space-between"} m={"15vh 10vw 15vh 15vw"}>
 
-                        <Box my={"2%"} textAlign={"start"}>
+                        <Box my={"2%"} textAlign={"start"} data-aos="fade-zoom-in">
                             <Heading fontSize={"48px"}>Impact Tracking</Heading>
                             <Heading fontSize={"48px"}>and Reporting</Heading>
                             <Text color="#363636" fontWeight="500" paddingRight={"40%"} mt={"3%"} textAlign={"start"}>
@@ -115,7 +120,7 @@ const HomeContent = () => {
                             </Button>
                         </Box>
 
-                        <Image src={img4} w={"40%"}></Image>
+                        <Image src={img4} w={"40%"} data-aos="fade-zoom-in"></Image>
                     </Flex>
                 </Flex>
             </Box>
